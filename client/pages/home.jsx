ac413f2c3ad2acb512a7772c169be6ee576eead2
Jimmy Vu <JMV3211@gmail.com>
2022-08-28 21:21:35 -0700

    Created HTML skeleton and set up root rendering for landing page #1

---
+++ client/pages/home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "../components/header";
+
+function Home(props) {
+  return (
+    <Header></Header>
+  );
+}
+
+export default Home;
---
+++ client/pages/not-found.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/header";
+
+function NotFound(props) {
+  return (
+    <>
+      <Header></Header>
+      <h1>NOT FOUND</h1>
+    </>
+  );
+}
+
+export default NotFound;
