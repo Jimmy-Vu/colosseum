31c3682dc1c5ad37e2cfa48e2fb59b5181b15bf0
Jimmy Vu <JMV3211@gmail.com>
2022-09-09 18:42:10 -0700

    WIP

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,18 +4,11 @@ import AppDrawer from "./app-drawer";
 function Header(props) {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-  function menuClickHandler() {
-    if (drawerIsOpen) {
-      setDrawerIsOpen(false);
-    } else {
-      setDrawerIsOpen(true);
-    }
-  }
   return (
     <header>
       <AppDrawer isOpen={drawerIsOpen}></AppDrawer>
       <div className="header">
-        <button onClick={menuClickHandler} type="button">
+        <button onClick={() => setDrawerIsOpen(prevState => !prevState)} type="button">
           <i className="menu-button fa-solid fa-bars"></i>
         </button>
         <h1 className="header__title"><a href="">COLOSSEUM</a></h1>
