688dc4a064f25473e82eb75e5b41a2a2d6ec7281
Jimmy Vu <JMV3211@gmail.com>
2022-09-17 10:38:15 -0700

    Set up form state changes #5

--- client/pages/create-listing.jsx
+++ client/pages/create-listing.jsx
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function CreateListing(props) {
+  const [inputs, setInputs] = useState({
+    name: '',
+    address: '',
+    type: {
+      commercial: false,
+      powerlifting: false,
+      weightlifting: false,
+      climbing: false,
+      boxing: false,
+      muaythai: false,
+      taekwondo: false,
+      karate: false,
+      bjj: false
+    },
+    image: ''
+  });
+
+  // useEffect(() => {
+  //   console.log(inputs);
+  // }, [inputs]);
 
   function handleSubmit(e) {
     e.preventDefault();
+    console.log(inputs);
   }
 
   function handleChange(e) {
+    setInputs(prev => ({ ...prev, [e.target.id]: e.target.value }));
+  }
 
+  function handleCheckboxes(e) {
+    setInputs(prev => ({
+      ...prev,
+      type: {
+        ...prev.type,
+        [event.target.id]: event.target.checked
+      }
+    }));
   }
 
   return (
     <main className="create-main">
-      <form className="create-form" onSubmit={handleSubmit}>
+      <form className="create-form" onSubmit={handleSubmit} encType="multipart/form-data">
         <div className="text-inputs">
           <label className="name-label" htmlFor="name">Name</label>
           <input className="name-input" onChange={handleChange} type="text" name="name" id="name" />
           <label className="address-label" htmlFor="address">Address</label>
           <input className="address-input" onChange={handleChange} type="text" name="address" id="address" />
         </div>
-        <fieldset className="specialization-fieldset">
+        <fieldset onChange={handleCheckboxes} id="type" className="specialization-fieldset">
           <legend>Choose the type of specialization(s) of the arena:</legend>
           <div className="checkbox-option">
             <input type="checkbox" name="commercial" id="commercial" />
@@ -42,7 +73,7 @@ function CreateListing(props) {
             <label htmlFor="boxing">Boxing</label>
           </div>
           <div className="checkbox-option">
-            <input type="checkbox" name="muay-thai" id="muay-thai" />
+            <input type="checkbox" name="muaythai" id="muaythai" />
             <label htmlFor="muay-thai">Muay Thai</label>
           </div>
           <div className="checkbox-option">
@@ -59,8 +90,8 @@ function CreateListing(props) {
           </div>
         </fieldset>
         <div className="upload-submit-container">
-          <label htmlFor="gym-image">Choose an image for the gym</label>
-          <input id="gym-image" type="file" accept="image/*" />
+          <label htmlFor="image">Choose an image for the gym</label>
+          <input onChange={handleChange} id="image" type="file" accept="image/*" />
           <button className="submit-button" type="submit">Submit</button>
         </div>
       </form>
