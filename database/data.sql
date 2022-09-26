e2f35f80027d7444d01920768367c257b98abf37
Jimmy Vu <JMV3211@gmail.com>
2022-09-26 07:31:38 -0700

    Added one more value for data.sql for initial directseed

--- database/data.sql
+++ database/data.sql
@@ -5,10 +5,18 @@ insert into "gyms" (
   "imageURL",
   "description"
   )
-  values (
+  values
+  (
     'Exceed Performance',
     '2060 S Euclid St Suite K, Anaheim, CA 92802',
     '{"powerlifting","weightlifting"}',
     'https://images.squarespace-cdn.com/content/v1/5f1121d3454d3d642df2c2a0/1594959199899-ZR1KADRBX6O6CCD2I3RR/IMG_8029.jpg?format=1500w',
     'Our mission at Exceed Performance is to cultivate and inspire individuals in all aspects of strength training, powerlifting, weightlifting, and overall health. We aspire to unlock the potential of our members both mentally and physically. We welcome everyone from novices to world class athletes and to share their journey of endless growth with us.'
+  ),
+  (
+    'Bayside Boxing',
+    'Torrance, CA',
+    'boxing',
+    'https://images.unsplash.com/photo-1561532325-7d5231a2dede?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80',
+    'Our mission is to train the warriors of tomorrow, no matter the gender, origins, or creed.'
   )
