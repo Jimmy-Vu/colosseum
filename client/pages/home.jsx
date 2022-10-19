57895d635101e83954d4cb0cb952801fd4abeb5a
Jimmy Vu <JMV3211@gmail.com>
2022-10-18 19:57:32 -0700

    Made changes to the home page

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import Header from "../components/header";
-import Footer from "../components/footer";
+import { useSelector } from "react-redux";
 
 function Home(props) {
+  const isLoggedIn = useSelector(state => state.app.isLoggedIn);
+  const username = useSelector(state => state.user.username);
+
   return (
     <div className="home-container">
       <main className="home-main">
-        {/* <section className="home-description">
-          <p>Welcome to Colosseum!</p>
-          <p>Your first steps into self betterment awaits.</p>
-        </section> */}
+        <section className="home-description">
+          {/* <p>{Welcome to Colosseum!}</p> */}
+          {/* <p>Your first steps into self betterment awaits.</p> */}
+        </section>
         <a className="home-search-btn" href="#listings">Find Your Arena</a>
       </main>
     </div>
