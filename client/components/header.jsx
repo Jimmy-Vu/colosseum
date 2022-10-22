72d1d014bcd6897fbdbcb7490aad2f4b5d01d786
Jimmy Vu <JMV3211@gmail.com>
2022-10-21 18:15:36 -0700

    Refactored css to handle scroll better and to change background color behavior of header #11

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -5,15 +5,32 @@ import { useSelector, useDispatch } from 'react-redux';
 
 function Header(props) {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+  const [navBarScrolled, setNavBarScrolled] = useState(false);
   const isLoggedIn = useSelector(state => state.app.isLoggedIn);
   const handleSignOut = props.handleSignOut;
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeBackground);
+    return () => {
+      window.removeEventListener('scroll', changeBackground);
+    };
+  }, [window.scrollY]);
+
+
+  function changeBackground() {
+    if (window.scrollY >= 50) {
+      setNavBarScrolled(true);
+    } else {
+      setNavBarScrolled(false);
+    }
+  }
 
   function handleSignInClick() {
     window.location.hash = '#auth';
   }
 
   return (
-    <header>
+    <header className={navBarScrolled ? 'scrolled' : ''}>
       <AppDrawer handleSignOut={handleSignOut} isOpen={drawerIsOpen} setDrawerIsOpen={setDrawerIsOpen}></AppDrawer>
       <div className="header">
         <button onClick={() => setDrawerIsOpen(prevState => !prevState)} type="button">
