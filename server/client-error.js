92afce3d35e32c0a35aa785690ad077c82dff17e
Jimmy Vu <JMV3211@gmail.com>
2023-01-03 19:51:45 -0800

    Removed compiled JS files

--- server/client-error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ClientError {
-    constructor(status, message) {
-        this.status = status;
-        this.message = message;
-    }
-}
-module.exports = ClientError;
--- server/db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pg = require('pg');
-const db = new pg.Pool({
-    connectionString: process.env.DATABASE_URL
-});
-module.exports = db;
