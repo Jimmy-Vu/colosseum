27d07e7d3faea46775b0c943eb25590e919ce171
Jimmy Vu <JMV3211@gmail.com>
2023-02-04 16:53:02 -0800

    Add additional dummy data and updated the test files

--- playwright-tests/home.spec.ts
+++ playwright-tests/home.spec.ts
@@ -7,24 +7,18 @@ test.describe('homepage navigation', () => {
   });
 
   test('homepage has title', async ({ page }) => {
-    // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Colosseum/);
 
   });
 
   test('homepage navigates successfully to listings page', async ({ page }) => {
-    // create a locator for the listing button
     const findYourArena = page.getByRole('link', { name: 'Find Your Arena' });
-    // Expect an attribute "to be strictly equal" to the value.
     await expect(findYourArena).toHaveAttribute('href', '#listings');
-    // Click the listings link.
     await findYourArena.click();
-    // Expects the URL to contain intro.
     await expect(page).toHaveURL(/.*listings/);
   })
 
   test('homepage navigates successfully to sign in page', async ({ page }) => {
-    // create a locator for the sign in button
     const signIn = page.getByRole('link', { name: 'Sign In' });
     await signIn.click();
     await expect(page).toHaveURL(/#auth/);
